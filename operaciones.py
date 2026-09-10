c36ffc4ffaa5195d512dd86a869aea38878408d4
Ignacio Lopez <nacholopez1903@gmail.com>
2026-09-10 11:36:13 -0300

    Agrego validar valor y mes, corrijo validar dia
    Me di cuenta que al validar mes tengo que pasar el texto a minusculas y despues aplicar title para que coincidan con los valores de la tupla. Tambien corregi dia que al final saque la opcion de numeros y no validaba lo que hacia con el mes.

--- operaciones.py
+++ operaciones.py
@@ -9,4 +9,10 @@ def validar_semana(semana):
     return semana in (1, 2, 3, 4)
 
 def validar_dia(dia):
-    return dia in DIAS or (type(dia) == int and 1 <= dia <= 5)
+    return type(dia) == str and dia.lower().title() in DIAS
+
+def validar_valor(valor):
+    return type(valor) in (int, float) and valor >= 0
+
+def validar_mes(mes):
+    return type(mes) == str and mes.lower().title() in MESES
